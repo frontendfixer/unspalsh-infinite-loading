d3c893c7bff3ca7122ea066a2fb3a6d97f0319e7
Lakshmikanta <frontendfixer@gmail.com>
2023-04-07 23:16:01 +0530

    final submit

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+const heading = document.querySelector('h1');
 const imgContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader');
+const BackToTop = document.getElementById('back-to-top');
+
 imgContainer.innerHTML = '';
 
-let imageCount = 20;
 let photosArr = [];
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 
 // Unsplash API
+let imageCount = 5;
+let imageOrientation = 'portrait';
 const apiKey = 'w2acEZ5xS4Po-MmlcKoK4TO68pdKg9glCoNIQnzr_h0';
-let unsplashApi = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imageCount}&orientation=landscape`;
 
 // Image loading function
 const imgLoaded = function () {
@@ -39,7 +42,7 @@ const displayPhotos = function () {
     });
     const img = document.createElement('img');
     setAttributes(img, {
-      src: photo.urls.regular,
+      src: photo.urls.thumb,
       alt: photo.alt_description,
       title: photo.description ? photo.description : photo.alt_description,
     });
@@ -47,19 +50,31 @@ const displayPhotos = function () {
     item.appendChild(img);
     imgContainer.appendChild(item);
     img.addEventListener('load', imgLoaded);
-    imageCount = 30;
   });
 };
 
+const errorElement = function (message) {
+  const html = `<img class="error-image" src="./404-Illustration.png" alt="" />
+  <div class="error-message">
+    catch an error with code:${message} while loading images 😔😔
+  </div>`;
+  imgContainer.insertAdjacentHTML('beforeend', html);
+  loader.hidden = true;
+};
 // Getphotos from unplash
 const getPhotos = async function () {
-  try {
-    const response = await fetch(unsplashApi);
-    photosArr = await response.json();
-    displayPhotos();
-  } catch (err) {
-    console.error(err);
+  if (window.innerWidth >= 480) {
+    imageOrientation = 'landscape';
   }
+  let unsplashApi = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imageCount}&orientation=${imageOrientation}`;
+  const response = await fetch(unsplashApi);
+  if (!response.ok) {
+    errorElement(response.status);
+  }
+
+  photosArr = await response.json();
+  displayPhotos();
+
   //   Check to see if scroling near bottom of page, then load more Photos
   window.addEventListener('scroll', () => {
     if (
@@ -67,6 +82,7 @@ const getPhotos = async function () {
         document.body.offsetHeight - 1440 &&
       ready
     ) {
+      imageCount = 20;
       ready = false;
       getPhotos();
     }
@@ -74,3 +90,21 @@ const getPhotos = async function () {
 };
 
 getPhotos();
+
+const ScrollToTop = function () {
+  document.addEventListener('scroll', () => {
+    window.scrollY > window.innerHeight
+      ? BackToTop.classList.add('active')
+      : BackToTop.classList.remove('active');
+  });
+
+  const headingHeight = heading.getBoundingClientRect().bottom;
+  BackToTop.addEventListener('click', () => {
+    window.scrollTo({
+      top: headingHeight,
+      behavior: 'smooth',
+    });
+  });
+};
+
+ScrollToTop();
